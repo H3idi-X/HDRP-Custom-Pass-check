2f310190f073c031b206a2628879cc4809215d6a
Toshiyuki Mori <toshiyuki@unity3d.com>
2020-05-12 11:47:00 +0900

    Added the scene data, scripts and shaders.

---
+++ Assets/Scripts/RotateMe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace UnityEngine.Experimental.Anime.HdrpCustomPostEdgeDetection
+{ 
+    public class RotateMe : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+    
+        }
+    
+        // Update is called once per frame
+        void Update () {
+            transform.Rotate(Vector3.up * 20 * Time.deltaTime);
+        }
+    }
+}
